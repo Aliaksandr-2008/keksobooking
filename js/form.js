767e64c434b8dc3ee326c79c2c56f2e489a82fe2
Sergey Kuzmin <kuzmins@inbox.ru>
2017-02-26 00:15:28 +0700

    Модуль 4 задание 1. Добавлены метки ARIA в разметку. Добавлены обработчики клавиш. Часть значений вынесена в константы.

--- js/form.js
+++ js/form.js
@@ -1,6 +1,18 @@
 'use strict';
 
 (function () {
+  var keyCodeList = {
+    ENTER: 13,
+    ESC: 27
+  };
+
+  var valueList = {
+    PRICE_MIN: 1000,
+    PRICE_MAX: 1000000,
+    TITLE_LENGTH_MIN: 30,
+    TITLE_LENGTH_MAX: 100
+  };
+
   var pinMap = document.querySelector('.tokyo__pin-map');
   var activePin;
   var dialog = document.querySelector('.dialog');
@@ -14,39 +26,79 @@
     }
   };
 
-  pinMap.addEventListener('click', function (evt) {
-    evt.preventDefault();
-
-    var target = evt.target;
-
-    deactivatePin();
-
-    while (target !== pinMap) {
-      if (target.classList.contains('pin') && target !== activePin) {
-        target.classList.add('pin--active');
-        dialog.classList.add('dialog--active');
-      }
-      target = target.parentNode;
+  // Активирует элемент на карте, по-которому был клик или нажатие клавиши "Enter"
+  var activatePin = function (target) {
+    if (target.classList.contains('pin') && target !== activePin) {
+      target.classList.add('pin--active');
+      dialog.classList.add('dialog--active');
     }
+  };
 
-    // Обработчик событий окна
-    var dialogHanler = function (e) {
-      e.preventDefault();
+  // Обработчик событий на карте
+  var pinMapHadler = function (evt) {
 
-      var trgt = e.target;
-
-      while (trgt !== dialog) {
-        if (trgt.classList.contains('dialog__close')) {
-          dialog.classList.remove('dialog--active');
-          dialog.removeEventListener('click', dialogHanler);
-          deactivatePin();
-        }
-        trgt = trgt.parentNode;
+    var enterCondition = function (e) {
+      if (e.type === 'click' || e.keyCode === keyCodeList.ENTER) {
+        return true;
+      } else {
+        return false;
       }
     };
 
+    if (enterCondition(evt)) {
+      evt.preventDefault();
+
+      var target = evt.target;
+
+      deactivatePin();
+
+      while (target !== pinMap) {
+        if (target.classList.contains('pin') && target !== activePin) {
+          activatePin(target);
+        }
+        target = target.parentNode;
+      }
+
+      // Закрывает окно диалога
+      var closeDialogAction = function (e) {
+        e.preventDefault();
+        dialog.classList.remove('dialog--active');
+        dialog.removeEventListener('click', dialogHanler);
+        dialog.removeEventListener('keydown', dialogHanler);
+        window.removeEventListener('keydown', escBtnHandler);
+        deactivatePin();
+      };
+
+      // Обработчик клавиши "ESC"
+      var escBtnHandler = function (e) {
+        if (e.keyCode === keyCodeList.ESC) {
+          closeDialogAction(e);
+        }
+      };
+
+      // Обработчик событий окна
+      var dialogHanler = function (e) {
+        if (enterCondition(e)) {
+
+
+          var trgt = e.target;
+
+          while (trgt !== dialog) {
+            if (trgt.classList.contains('dialog__close')) {
+              closeDialogAction(e);
+            }
+            trgt = trgt.parentNode;
+          }
+        }
+      };
+    }
+
     dialog.addEventListener('click', dialogHanler);
-  });
+    window.addEventListener('keydown', escBtnHandler);
+  };
+
+  pinMap.addEventListener('click', pinMapHadler);
+  pinMap.addEventListener('keydown', pinMapHadler);
 
   var noticeForm = document.querySelector('.notice__form');
   var noticeTitle = noticeForm.querySelector('#title');
@@ -55,11 +107,11 @@
 
   // Установка требований к данным, вводимым в поля формы
   noticeTitle.required = true;
-  noticeTitle.minLength = 30;
-  noticeTitle.maxLength = 100;
+  noticeTitle.minLength = valueList.TITLE_LENGTH_MIN;
+  noticeTitle.maxLength = valueList.TITLE_LENGTH_MAX;
   noticePrice.required = true;
-  noticePrice.min = 1000;
-  noticePrice.max = 1000000;
+  noticePrice.min = valueList.PRICE_MIN;
+  noticePrice.max = valueList.PRICE_MAX;
   noticeAddress.required = true;
 
   var roomNumber = noticeForm.querySelector('#room_number');
